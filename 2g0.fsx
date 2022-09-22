8e133d74ceabe82fe2cc6e60aefb6838a9e239aa
mikkellhf <prd882@alumni.ku.dk>
2022-09-22 13:56:01 +0200

    Update 2g0.fsx

--- 2g0.fsx
+++ 2g0.fsx
@@ -1,4 +1,14 @@
+
 type vec = float * float
+
+
+///<summary>
+/// Given parameters vectorA and vectorB, this function will add them together and output a new vec.
+/// when vectorA = (1.1 , 1.2) and vectorB = (1.3 , 1.5), the output of add will be v3 = (1.4 , 1.7)
+/// </summary> 
+/// <param name = "vectorA" > A tuple in the type vec. </param>
+/// <param name = "vectorB" > A 2nd tuple in the type vec. </param>
+/// <returns> The added vector, named v3, which is the two param's added together. </param>
 
 let add (v1:vec) (v2:vec) : (vec) =
     let v3A = fst v1 + fst v2
